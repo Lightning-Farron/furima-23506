d4fc77a9148ea5b4d8c4c29d73a534ebb841b020
Lightning-farron <us2kxgxp@icloud.com>
2021-01-23 13:50:49 +0900

    ユーザー管理機能、修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,16 +9,7 @@ FactoryBot.define do
     last_name_reading     {"ヤマダ"}
     first_name_reading    {"タロウ"}
     birthday              {"2011-03-21"}
-    # { Faker::Date.between_except(from: 20.year.ago, to: 1.year.from_now, excepted: Date.today) }
   end
 end
 
 
-# nickname              {Faker::Name.initials(number: 2)}
-# email                 {Faker::Internet.free_email}
-# password              {Faker::Internet.password(min_length: 6)}
-# password_confirmation {password}
-# Faker::Time
-
-
-# validates :password, presence: true, length: { minimum: 6 }
