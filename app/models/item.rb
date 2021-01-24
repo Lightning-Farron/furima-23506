40e0cfa10b1c19ab80d62281e20f3ad29cbd77c3
Lightning-farron <us2kxgxp@icloud.com>
2021-01-24 21:36:01 +0900

    商品出品機能実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,26 @@
 class Item < ApplicationRecord
   has_one_attached :image
+  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category_id
-  belongs_to :condition_id
-  belongs_to :shipping_charge_id
-  belongs_to :prefectures_id
-  belongs_to :pays_to_ship_id
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_charge
+  belongs_to :prefecture
+  belongs_to :pays_to_ship
+
+  with_options presence: true do
+    validates :image
+    validates :name
+    validates :description
+  end  
+
+  with_options presence: true, numericality: { other_than: 0 } do
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefectures_id
+    validates :pays_to_ship_id
+  end
+
+  validates :price, presence: true, format: { with: /\A[0-9]+\z/ }, length: { minimum: 3, maxinum: 7 }, numericality: { only_integer: true, greater_than: 300, less_than: 10000000 }
 end
