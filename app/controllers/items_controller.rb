86ee95768a3bcc108314380bf58ab5ca177ecdd0
Lightning-farron <us2kxgxp@icloud.com>
2021-03-17 20:09:29 +0900

    複数枚画像投稿の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,7 +46,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :pays_to_ship_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :pays_to_ship_id, :price, images: []).merge(user_id: current_user.id)
   end
 
   def set_item
