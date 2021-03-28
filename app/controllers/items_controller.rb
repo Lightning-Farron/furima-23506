2d06736a95b67c473bc2dd50ffc7f3c4270c01ad
Lightning-farron <us2kxgxp@icloud.com>
2021-03-28 20:54:20 +0900

    タグ付け投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,15 +8,16 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @item = Item.new
+    @item_tag = ItemsTag.new
   end
 
-  def create   
-      @item = Item.new(item_params)
-      if @item.valid?
-        @item.save
+  def create
+      @item_tag = ItemsTag.new(item_params)
+      if @item_tag.valid?
+        @item_tag.save
         redirect_to root_path
       else
+
         render :new
       end
   end
@@ -30,6 +31,14 @@ class ItemsController < ApplicationController
     else
       render :edit
     end
+    #タグの編集
+    # @item_tag = ItemsTag.update(edit_params)
+    # if @item_tag.valid?
+    #   @item_tag.save
+    #   redirect_to root_path
+    # else
+    #   render :edit
+    # end
   end
 
   def show
@@ -43,14 +52,29 @@ class ItemsController < ApplicationController
     end
   end
 
+  def search
+    return nil if params[:keyword] == ""
+    tag = Tag.where(['tag_name LIKE ?', "%#{params[:keyword]}%"] )
+    render json:{ keyword: tag }
+  end
+
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :pays_to_ship_id, :price, images: []).merge(user_id: current_user.id)
+    params.require(:items_tag).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :pays_to_ship_id, :price, :tag_name, images: []).merge(user_id: current_user.id)
   end
-
+#タグの編集
+  # def edit_params
+  #   params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :pays_to_ship_id, :price, images: []).merge(user_id: current_user.id)
+  # end
+  
   def set_item
     @item = Item.find(params[:id])
+#タグの編集
+    # @item_tag_relations = @item.item_tag_relations
+    # @item_tag_relations.each do |item_tag_relation|
+    #   tag = Tag.find(item_tag_relation.tag_id)
+    # end
   end
 
   def ensure_correct_user
